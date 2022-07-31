e884d7b8ddb0250f90820801f40b3afb86abf740
Lukas <lukasjankauskasthelj@gmail.com>
2022-07-31 15:58:06 +0300

    re: prev commit

--- src/main/java/nationalid/loggers/ConsoleLogger.java
+++ src/main/java/nationalid/loggers/ConsoleLogger.java
@@ -6,7 +6,7 @@ public class ConsoleLogger implements Logable {
 
     @Override
     public void LogMessage(String message) throws Exception {
-        System.out.print(message);
+        System.err.print(message);
     }
 
     @Override
